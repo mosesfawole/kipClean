88c9380171cb3448b3af6cf5ef2deb0876ace170
chibuike mokwe <mokwechibuike7@gmail.com>
2024-06-21 10:54:58 +0100

    all changes made

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -1,43 +1,84 @@
 const ChooseUs = () => {
-  return (
-    <>
-      <div>
-        <div className="px-6 py-4 text-center bg-[#FFF4EC]">
-          <h1 className="text-xl font-bold text-center">Why Choose Us?</h1>
-          <p className="pt-4">
-            Kip-Clean Cleaning and Plumbing Service has established itself as a
-            beacon of reliability and excellence in the industry. Through a
-            commitment to unparalleled service and customer satisfaction,
-            Kip-Clean has consistently exceeded the expectations of its clients.
-          </p>
-        </div>
-        {/* Grid Section */}
-        <div className="grid h-fit grid-cols-1 gap-4 mx-8 md:grid-cols-2 lg:grid-rows-2">
-          <div className="relative w-full h-full lg:row-span-1">
-            <img
-              src="https://plus.unsplash.com/premium_photo-1674169166921-322a3fdb9f8b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Image 2"
-              className="object-cover w-full h-full rounded-xl"
-            />
-          </div>
-          <div className="relative order-3 w-full h-full lg:col-span-1  md:row-span-2 md:order-2">
-            <img
-              src="https://images.unsplash.com/photo-1617329516256-c04932f85ac9?q=80&w=1430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Image 1"
-              className="object-cover w-full h-full rounded-xl"
-            />
-          </div>
-          <div className="relative order-2 w-full h-full lg:row-span-1 md:order-3">
-            <img
-              src="https://plus.unsplash.com/premium_photo-1683134474181-8b88c82b6aa8?q=80&w=1477&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="Image 3"
-              className="object-cover w-full h-full rounded-xl"
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='mt-[7rem]'>
+				<div className='px-6 py-4 text-center bg-[#FFF4EC]'>
+					<h1 className='text-xl sm:text-4xl font-bold text-center'>
+						Why Choose Us?
+					</h1>
+					<p className='pt-4 text-sm sm:text-base sm:w-[60%] text-center mx-auto'>
+						Kip-Clean Cleaning and Plumbing Service has established itself as a
+						beacon of reliability and excellence in the industry. Through a
+						commitment to unparalleled service and customer satisfaction,
+						Kip-Clean has consistently exceeded the expectations of its clients.
+					</p>
+				</div>
+				{/* Grid Section */}
+				<div className='grid grid-cols-1 gap-7 mx-4 md:mx-[5rem] lg:grid-cols-2 lg:grid-rows-2 mt-7 h-[45rem] text-white'>
+					<div className='relative w-full h-full sm:h-[20rem] lg:h-full lg:row-span-1 bg-img3 bg-cover bg-center rounded-lg md:rounded-[2.5rem] shadow-black/50 shadow-xl p-4 sm:p-8'>
+						<div className='w-full h-full sm:h-[20rem] lg:h-full rounded-lg md:rounded-[2.5rem] bg-black/60 absolute top-0 left-0'></div>
+						<div className='flex flex-col justify-between relative h-full'>
+							<div>
+								<img
+									className='w-[2rem] md:w-[4rem]'
+									src='/Vector1.png'
+									alt=''
+								/>
+							</div>
+
+							<div className='flex flex-col sm:gap-4'>
+								<p className='text-2xl sm:text-4xl '>Same Day Availability</p>
+								<p className=' text-sm sm:text-xl'>
+									Emergency Service needed? We provide immediate booking
+									confirmation for your cleaning service with us.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className='relative order-3 w-full h-full sm:h-[20rem] lg:h-full lg:col-span-1  md:row-span-2 md:order-2 bg-img2 bg-cover bg-center rounded-lg md:rounded-[2.5rem] shadow-black/50 shadow-xl p-4 md:p-8'>
+						<div className='w-full h-full sm:h-[20rem] lg:h-full rounded-lg md:rounded-[2.5rem] bg-black/60 absolute top-0 left-0'></div>
+						<div className='flex flex-col justify-between relative h-full'>
+							<div>
+								<img
+									className='w-[2rem] md:w-[4rem]'
+									src='/Vector3.png'
+									alt=''
+								/>
+							</div>
+
+							<div className='flex flex-col sm:gap-4'>
+								<p className='text-2xl sm:text-4xl '>Quick Response</p>
+								<p className=' text-sm sm:text-xl'>
+									For Emergency cleaning and plumbing services, you can count on
+									us to respond swiftly and get the job done right.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className='relative order-2 w-full h-full sm:h-[20rem] lg:h-full lg:row-span-1 md:order-3 bg-img1 bg-cover bg-center rounded-lg md:rounded-[2.5rem] shadow-black/50 shadow-xl p-4 md:p-8'>
+						<div className='w-full h-full sm:h-[20rem] lg:h-full rounded-lg md:rounded-[2.5rem] bg-black/60 absolute top-0 left-0'></div>
+						<div className='flex flex-col justify-between relative h-full'>
+							<div>
+								<img
+									className='w-[2rem] md:w-[4rem]'
+									src='/Vector2.png'
+									alt=''
+								/>
+							</div>
+
+							<div className='flex flex-col sm:gap-4'>
+								<p className='text-2xl sm:text-4xl '>Great Feedback</p>
+								<p className=' text-sm sm:text-xl'>
+									We have a great customer return rate and most would recommend
+									us to a friend or family member.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ChooseUs;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,129 +1,121 @@
-import { useState } from "react";
-import { MdMenu } from "react-icons/md";
-import { IoClose } from "react-icons/io5";
-import { Link } from "react-router-dom";
-import { IoIosArrowDown } from "react-icons/io";
+import { useState } from 'react';
+import { MdMenu } from 'react-icons/md';
+import { IoClose } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { IoIosArrowDown } from 'react-icons/io';
 
 const Nav = () => {
-  const [active, setActive] = useState("Home");
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [dropDown, setDropDown] = useState(false);
-  const navLists = [
-    { name: "Home", path: "/" },
-    { name: "About Us", path: "/about" },
-    { name: "Services", path: "", arrow: <IoIosArrowDown /> },
-    { name: "Contact Us", path: "/contact" },
-  ];
+	const [active, setActive] = useState('Home');
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [dropDown, setDropDown] = useState(false);
+	const navLists = [
+		{ name: 'Home', path: '/' },
+		{ name: 'About Us', path: '/about' },
+		{ name: 'Services', path: '/services', arrow: <IoIosArrowDown /> },
+		{ name: 'Contact Us', path: '/contact' },
+	];
 
-  return (
-    <div className="flex items-center justify-between px-8 py-4">
-      <div>
-        <Link to="/">
-          <img src="/logo.png" alt="" className="w-12 h-12 " />
-        </Link>
-      </div>
-      <div className="items-center hidden gap-5 md:flex">
-        <ul className="flex items-center gap-12">
-          {navLists.map((navList, index) => (
-            <li
-              key={index}
-              className="relative cursor-pointer"
-              onMouseEnter={() => setActive(navList.name)}
-            >
-              <Link to={navList.path}>
-                <div className="relative flex items-center gap-8 ">
-                  <span
-                    className={
-                      active === navList.name
-                        ? "text-[#CC5500]"
-                        : "text-[#7D7D7D] transition duration-500"
-                    }
-                  >
-                    {navList.name}
-                  </span>
-                  <div
-                    className=" absolute right-[-40%]"
-                    onClick={() => setDropDown((prevMode) => !prevMode)}
-                  >
-                    {navList.arrow ? navList.arrow : null}
-                  </div>
-                </div>
+	return (
+		<div className='flex items-center justify-between px-8 sm:px-[4rem] py-4 max-w-[1440px] mx-auto'>
+			<div>
+				<Link to='/'>
+					<img src='/logo.png' alt='' className='w-12 h-12 ' />
+				</Link>
+			</div>
+			<div className='items-center hidden gap-5 md:flex'>
+				<ul className='flex items-center gap-12'>
+					{navLists.map((navList, index) => (
+						<li
+							key={index}
+							className='relative cursor-pointer'
+							onMouseEnter={() => setActive(navList.name)}>
+							<Link to={navList.path}>
+								<div className='relative flex items-center gap-8 '>
+									<span
+										className={
+											active === navList.name
+												? 'text-[#CC5500]'
+												: 'text-[#7D7D7D] transition duration-500'
+										}>
+										{navList.name}
+									</span>
+									<div
+										className=' absolute right-[-40%]'
+										onClick={() => setDropDown((prevMode) => !prevMode)}>
+										{navList.arrow ? navList.arrow : null}
+									</div>
+								</div>
 
-                {active === navList.name && (
-                  <span className="absolute left-0 right-0 -bottom-1 h-0.5 bg-[#CC5500]"></span>
-                )}
-              </Link>
-            </li>
-          ))}
-          {/*  */}
-          {dropDown && (
-            <div className=" absolute right-[42%] z-50 top-16 p-4 bg-black text-gray-500">
-              <Link to="/pricing">
-                <h1>Pricing</h1>
-              </Link>
-              <Link to="/services">
-                <h1>Services</h1>
-              </Link>
-            </div>
-          )}
-        </ul>
-      </div>
-      <button className="bg-[#CC5500] text-white rounded-3xl text-sm font-normal px-4 py-2">
-        Get a Quote
-      </button>
-      <div className="relative z-50 md:hidden">
-        {menuOpen ? (
-          <IoClose
-            color="white"
-            size={30}
-            className="absolute text-white bg-white cursor-pointer "
-            onClick={() => setMenuOpen(false)}
-          />
-        ) : (
-          <MdMenu
-            size={30}
-            className="cursor-pointer"
-            onClick={() => setMenuOpen(true)}
-          />
-        )}
-      </div>
-      {/* Hamburger menu */}
-      {menuOpen && (
-        <div className="absolute top-0 left-0 z-50 w-full h-[400px] shadow-lg bg-ash">
-          <div className="absolute border border-white rounded-full cursor-pointer top-8 right-8">
-            <IoClose
-              color="white"
-              size={25}
-              onClick={() => setMenuOpen(false)}
-            />
-          </div>
-          <ul className="flex flex-col gap-8 px-4 py-16 text-center">
-            {navLists.map((navList, index) => (
-              <>
-                <li
-                  key={index}
-                  className="text-white cursor-pointer"
-                  onClick={() => {
-                    setActive(navList.name);
-                    setMenuOpen(false);
-                  }}
-                >
-                  <Link to={navList.path}>{navList.name}</Link>
-                </li>
-                {/* <span className=" bg-white w-full h-[1px]"></span> */}
-                {/* <hr className="text-white bg-white " /> */}
-              </>
-            ))}
-            <li>
-              <button className="bg-[#CC5500] text-white rounded-3xl text-sm font-normal px-4 py-2">
-                Get a Quote
-              </button>
-            </li>
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+								{active === navList.name && (
+									<span className='absolute left-0 right-0 -bottom-1 h-0.5 bg-[#CC5500]'></span>
+								)}
+							</Link>
+						</li>
+					))}
+					{/*  */}
+					{dropDown && (
+						<div className=' absolute right-[42%] z-50 top-16 p-4 bg-black text-gray-500'>
+							<Link to='/pricing'>
+								<h1>Pricing</h1>
+							</Link>
+							<Link to='/services'>
+								<h1>Services</h1>
+							</Link>
+						</div>
+					)}
+				</ul>
+			</div>
+			<button className='bg-[#CC5500] text-white rounded-3xl text-sm font-normal px-4 py-2'>
+				Get a Quote
+			</button>
+			<div className='relative z-50 md:hidden'>
+				{menuOpen ? (
+					<IoClose
+						color='white'
+						size={30}
+						className='absolute text-white bg-white cursor-pointer '
+						onClick={() => setMenuOpen(false)}
+					/>
+				) : (
+					<MdMenu
+						size={30}
+						className='cursor-pointer'
+						onClick={() => setMenuOpen(true)}
+					/>
+				)}
+			</div>
+			{/* Hamburger menu */}
+			{menuOpen && (
+				<div className='absolute top-0 left-0 z-50 w-full h-[400px] shadow-lg bg-ash'>
+					<div className='absolute border border-white rounded-full cursor-pointer top-8 right-8'>
+						<IoClose
+							color='white'
+							size={25}
+							onClick={() => setMenuOpen(false)}
+						/>
+					</div>
+					<ul className='flex flex-col gap-8 px-4 py-16 text-center'>
+						{navLists.map((navList, index) => (
+							<li
+								key={index}
+								className='text-white cursor-pointer'
+								onClick={() => {
+									setActive(navList.name);
+									setMenuOpen(false);
+								}}>
+								<Link to={navList.path}>{navList.name}</Link>
+							</li>
+						))}
+						<li>
+							<button className='bg-[#CC5500] text-white rounded-3xl text-sm font-normal px-4 py-2'>
+								Get a Quote
+							</button>
+						</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Nav;
